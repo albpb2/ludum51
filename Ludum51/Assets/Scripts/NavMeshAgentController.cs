e5b706e6bd14c5172d2c4846129cfe2d2d83f0e1
Meguipascual <meguipascual@gmail.com>
2022-10-01 19:06:59 +0200

    W, A, S, D and arrows' player controller.

--- Ludum51/Assets/Scripts/NavMeshAgentController.cs
+++ Ludum51/Assets/Scripts/NavMeshAgentController.cs
@@ -5,24 +5,22 @@ using UnityEngine.AI;
 
 public class NavMeshAgentController : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    RaycastHit hit;
+    Rigidbody rigidbody;
+    [SerializeField] float m_Speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-            {
-                agent.destination = hit.point;
-            }
-        }
+        //Store user input as a movement vector
+        Vector3 playerInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        rigidbody.MovePosition(transform.position + playerInput * Time.deltaTime * m_Speed);
     }
 }
