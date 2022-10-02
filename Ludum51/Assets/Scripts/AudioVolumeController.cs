946241adab68aed6041d6cc0f41fc3f50653de7c
albpb2 <albpb2@gmail.com>
2022-10-02 10:44:29 +0200

    Audio mixer

---
+++ Ludum51/Assets/Scripts/AudioVolumeController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioVolumeController : MonoBehaviour
+{
+    private const float volumeChangeStep = .05f;
+
+    [SerializeField] string _audioMixerGroupVolumeName;
+    [SerializeField] private AudioMixer _audioMixer;
+
+    public void IncreaseVolume()
+    {
+        _audioMixer.GetFloat(_audioMixerGroupVolumeName, out var currentVolume);
+        currentVolume = Mathf.Pow(10, currentVolume / 20);
+        var newVolume = Mathf.Min(1, currentVolume + volumeChangeStep);
+        Debug.Log($"Volume: {newVolume}");
+        newVolume = Mathf.Log10(newVolume) * 20;
+        _audioMixer.SetFloat(_audioMixerGroupVolumeName, newVolume);
+    }
+
+    public void ReduceVolume()
+    {
+        _audioMixer.GetFloat(_audioMixerGroupVolumeName, out var currentVolume);
+        currentVolume = Mathf.Pow(10, currentVolume / 20);
+        var newVolume = Mathf.Max(.00001f, currentVolume - volumeChangeStep);
+        Debug.Log($"Volume: {newVolume}");
+        newVolume = Mathf.Log10(newVolume) * 20;
+        _audioMixer.SetFloat(_audioMixerGroupVolumeName, newVolume);
+    }
+}
