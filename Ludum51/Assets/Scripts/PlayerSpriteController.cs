07f2390b7ed03e9fc04e6236056d1377c835a2da
albpb2 <albpb2@gmail.com>
2022-10-03 00:04:36 +0200

    Calculate mouse position in world only once per frame

--- Ludum51/Assets/Scripts/PlayerSpriteController.cs
+++ Ludum51/Assets/Scripts/PlayerSpriteController.cs
@@ -5,19 +5,11 @@ public class PlayerSpriteController : MonoBehaviour
     [SerializeField] private SpriteRenderer _characterSpriteRenderer;
     [SerializeField] private SpriteRenderer _gunSpriteRenderer;
 
-    private Plane plane = new Plane(Vector3.up, 0);
-
     private void Update()
     {
-        float distance;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (plane.Raycast(ray, out distance))
-        {
-            var mousePositionInWorld = ray.GetPoint(distance);
-            mousePositionInWorld = new Vector3(mousePositionInWorld.x, transform.position.y, mousePositionInWorld.z);
+        var flip = MousePositionInWorld.Instance.Value.x < transform.position.x;
 
-            _characterSpriteRenderer.flipX = mousePositionInWorld.x < transform.position.x;
-            _gunSpriteRenderer.flipX = mousePositionInWorld.x < transform.position.x;
-        }
+        _characterSpriteRenderer.flipX = flip;
+        _gunSpriteRenderer.flipX = flip;
     }
 }
