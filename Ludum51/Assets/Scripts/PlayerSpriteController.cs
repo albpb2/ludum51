99ce97c8d55f9fba5d943923437675f2055e30ee
albpb2 <albpb2@gmail.com>
2022-10-02 23:57:54 +0200

    Flip player and gun sprite according to cursor position

---
+++ Ludum51/Assets/Scripts/PlayerSpriteController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerSpriteController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _characterSpriteRenderer;
+    [SerializeField] private SpriteRenderer _gunSpriteRenderer;
+
+    private Plane plane = new Plane(Vector3.up, 0);
+
+    private void Update()
+    {
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            var mousePositionInWorld = ray.GetPoint(distance);
+            mousePositionInWorld = new Vector3(mousePositionInWorld.x, transform.position.y, mousePositionInWorld.z);
+
+            _characterSpriteRenderer.flipX = mousePositionInWorld.x < transform.position.x;
+            _gunSpriteRenderer.flipX = mousePositionInWorld.x < transform.position.x;
+        }
+    }
+}
