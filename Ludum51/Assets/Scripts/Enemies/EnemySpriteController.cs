c11b2366966f3d1acfa4aa29f0a1a796ed5eee81
albpb2 <albpb2@gmail.com>
2022-10-03 20:19:59 +0200

    Flip enemy sprites instead of rotating

---
+++ Ludum51/Assets/Scripts/Enemies/EnemySpriteController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySpriteController : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _enemyNavMeshAgent;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _spriteRenderer.flipX = _enemyNavMeshAgent.velocity.x < 0;
+    }
+}
